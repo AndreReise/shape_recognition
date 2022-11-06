75a83373060e720e627d033a901bdb9cf85af6a6
Andrii Babanin <andrii.babanin@gmail.com>
2022-11-06 14:00:40 +0200

    created directory to store k-nearest neighbors results;

--- 2. Digit recognition/source/source.py
+++ 2. Digit recognition/source/source.py
@@ -1,5 +1,5 @@
 import cv2
-import os
+import os, shutil
 import math
 import operator
 from scipy.ndimage import interpolation as inter
@@ -47,35 +47,40 @@ def calculateEuclideanDistance(image1, image2):
         print('Shapes are different')
         return 1000000
 
-    distance: float = 0
-
-    for x in range(0, image1.shape[0] - 1):
-        for y in range(0 , image1.shape[1] - 1):
-            pixel1 = image1[x][y]
-            pixel2 = image2[x][y]
-
-            if pixel1 != pixel2:
-                distance += math.pow((pixel1 - pixel2), 2)
+    distance = np.sum((image1 - image2) ** 2)
     
     return math.sqrt(distance)
+
+class ClassificationResult:
+    recognizedDigit: str
+    bitmapIndex: int
+    euclideanDistance: float
+
+    def __init__(self, recognizedDigit, bitmapIndex, euclideanDistance):
+        self.recognizedDigit = recognizedDigit
+        self.bitmapIndex = bitmapIndex
+        self.euclideanDistance = euclideanDistance
+
 
 def classify(preparedImage):
     distances = []
 
     for digit in data:
-        for testBitmap in data[digit]:
-            distance = calculateEuclideanDistance(preparedImage, testBitmap)
-
-            distances.append([digit, distance])
+        for bitmapIndex, bitmap in enumerate(data[digit]):
+            distance = calculateEuclideanDistance(preparedImage, bitmap)
+            
+            classificationResult = ClassificationResult(digit, bitmapIndex, distance)
+            distances.append(classificationResult)
     
-    distances.sort(key = lambda x: x[1])
+    distances.sort(key = lambda result: result.euclideanDistance)
+
     minDistances = distances[:8]
 
     frequency = dict()
 
     for distance in minDistances:
-        currentValue = frequency.get(distance[0], 0)
-        frequency[distance[0]] = currentValue + 1
+        currentValue = frequency.get(distance.recognizedDigit, 0)
+        frequency[distance.recognizedDigit] = currentValue + 1
 
     max_value = max(frequency.items(), key=operator.itemgetter(1))
 
@@ -88,14 +93,23 @@ def classify(preparedImage):
     if len(max_value_elements) == 1:
 
         for distance in minDistances:
-            if distance[0] == max_value_elements[0]:
-                return distance
+            if distance.recognizedDigit == max_value_elements[0]:
+                return distance.recognizedDigit, distance.euclideanDistance, minDistances
 
-    return (minDistances[0][0], minDistances[0][1])
+    return (minDistances[0].recognizedDigit, minDistances[0].euclideanDistance, minDistances)
 
 
+# prepare artifactgs directory
+artifactsPath = os.path.join(os.curdir, "artifacts")
 
-image = cv2.imread("sample2.png", cv2.IMREAD_GRAYSCALE)
+if os.path.isdir(artifactsPath):
+    shutil.rmtree(artifactsPath)
+    os.mkdir(artifactsPath)
+else:
+    os.mkdir(artifactsPath)
+
+sampleName = "sample3.png"
+image = cv2.imread(sampleName, cv2.IMREAD_GRAYSCALE)
 
 # In OpenCV, finding contours is like finding white object from black background.
 image = cv2.bitwise_not(image)
@@ -104,7 +118,7 @@ th, dst = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY);
 
 contours, _ = cv2.findContours(image=dst, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
 
-image_copy =  cv2.imread("sample2.png")
+image_copy =  cv2.imread(sampleName)
 
 count = 1
 
@@ -150,10 +164,18 @@ for contour in contours:
     _, thresh = cv2.threshold(centroidBased, 120, 255, cv2.THRESH_BINARY)
     thresh = thresh.astype(int) / 255
 
-    cv2.imwrite("{count}.jpg".format(count = count), centroidBased)
+    cv2.imwrite("artifacts/{count}.jpg".format(count = count), centroidBased)
 
     # 6. Apply classifier
-    classificationResult, distance = classify(thresh)
+    classificationResult, distance, neighbours = classify(thresh)
+
+    # dump nearest neighbours
+    for resultIndex, result in enumerate(neighbours):
+        bitmap = data[result.recognizedDigit][result.bitmapIndex]
+
+        #cv2.imshow('result', bitmap)
+        cv2.imwrite("artifacts/{count}-{index}.png".format(count = count, index=resultIndex), bitmap, params=[cv2.IMWRITE_PNG_BILEVEL])
+        #cv2.waitKey()
     #classificationResult = '1'
 
     #  Optional - some result writing
